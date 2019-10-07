4c9691b8f4fef78ec367bc41be11c6a57a4881b9
sergiobizini <sergiobizini@gmail.com>
2019-10-07 18:26:06 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,7 +13,24 @@ output.innerHTML += "typo ball1: " + typeof balls1 + "\n";
 var maxX = garden.clientWidth  - ball.clientWidth;
 var maxY = garden.clientHeight - ball.clientHeight;
 
+if (window.DeviceOrientationEvent){
+window.addEventListener("deviceorientation",function(event) {
+//Rotation around Z-axis
+  var rotateDegrees = event.alpha;
+//Left to right
+  var leftToRight = event.gamma;
+//front back motion
+var frontToBack = event.beta;
 
+handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+},true);
+}
+
+var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
+  output.innerHTML  = "alpha : " + rotateDegrees + "\n";
+  output.innerHTML += "gamma: " + leftToRight + "\n";
+  output.innerHTML += "beta: " + frontToBack + "\n";
+};
 
 
 //     console.log("b1: "+ball1.clientHeight+"\n"+"b2: "+ball2.clientHeight+"\n"+"b3: "+ball3.clientHeight+"\n");
@@ -21,36 +38,36 @@ var maxY = garden.clientHeight - ball.clientHeight;
 
 // function handleOrientation(event) {
 
-  balls.forEach((ball) => {
-  ball.addEventListener('deviceorientation', () => {
-    // function(ball, currentIndex, listObj) {
-
-
-var x = event.beta;  // In degree in the range [-180,180]
-var y = event.gamma; // In degree in the range [-90,90]
-
-//output.innerHTML  = "beta : " + x + "\n";
-//output.innerHTML += "gamma: " + y + "\n";
-
-// Because we don't want to have the device upside down
-// We constrain the x value to the range [-90,90]
-if (x >  90) { x =  90};
-if (x < -90) { x = -90};
-
-// To make computation easier we shift the range of
-// x and y to [0,180]
-x += 90;
-y += 90;
-
-// 10 is half the size of the ball
-// It center the positioning point to the center of the ball
-ball.style.top  = (maxX*x/180 - 10) + "px";
-ball.style.left = (maxY*y/180 - 10) + "px";
-
-      // console.log(ball + ', ' + currentIndex);
-      // output.innerHTML += "typo ball: " + typeof ball + "\n";
-    }
-  );
-});
+//   balls.forEach((ball) => {
+//   ball.addEventListener('deviceorientation', () => {
+//     // function(ball, currentIndex, listObj) {
+//
+//
+// var x = event.beta;  // In degree in the range [-180,180]
+// var y = event.gamma; // In degree in the range [-90,90]
+//
+// //output.innerHTML  = "beta : " + x + "\n";
+// //output.innerHTML += "gamma: " + y + "\n";
+//
+// // Because we don't want to have the device upside down
+// // We constrain the x value to the range [-90,90]
+// if (x >  90) { x =  90};
+// if (x < -90) { x = -90};
+//
+// // To make computation easier we shift the range of
+// // x and y to [0,180]
+// x += 90;
+// y += 90;
+//
+// // 10 is half the size of the ball
+// // It center the positioning point to the center of the ball
+// ball.style.top  = (maxX*x/180 - 10) + "px";
+// ball.style.left = (maxY*y/180 - 10) + "px";
+//
+//       // console.log(ball + ', ' + currentIndex);
+//       // output.innerHTML += "typo ball: " + typeof ball + "\n";
+//     }
+//   );
+// });
 
 // window.addEventListener('deviceorientation', handleOrientation);
