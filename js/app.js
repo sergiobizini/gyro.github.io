d5e0dbbaa352e0db9c1f622c966f1d7b561f8a8b
sergiobizini <sergiobizini@gmail.com>
2019-10-14 19:10:50 +0300

    s

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ function handleOrientation(event) {
   // It center the positioning point to the center of the ball
   // balls[i].style.top  = (maxX*x/180 - 10) + (i-1)*45 + "px";
   // balls[i].style.left = (maxY*y/180 - 10) + "px";
-  balls[i].style.top  = maxX - maxX*x/90 + "px";
+  balls[i].style.top  = 200 - 200*x/90 + "px";
   balls[i].style.left = (maxY*y/180 + 80) + "px";
     }
 }
