8be70850ad94c514d53fb278a587d7dcceeee18d
sergiobizini <sergiobizini@gmail.com>
2019-10-16 19:23:02 +0300

    d

--- js/app.js
+++ js/app.js
@@ -12,20 +12,20 @@ function handleOrientation(event) {
 
   var maxX = garden.clientWidth  - balls[i].clientWidth;
   var maxY = garden.clientHeight - balls[i].clientHeight;
-  var speed =  0.1;
+  var speed =  0.001;
 
 
   var y = event.beta;  // In degree in the range [-180,180]    up down
   var x = event.gamma; // In degree in the range [-90,90]      left right
 
-  output.innerHTML  = "top : " + y + "\n";
-  output.innerHTML += "left: " + x + "\n";
+  output.innerHTML  = "top : " + balls[i].style.top + "\n";
+  output.innerHTML += "left: " + balls[i].style.left + "\n";
   // output.innerHTML += "alpha: " + event.alpha + "\n";
   // output.innerHTML += "ball1: " + balls[0] + "ball2: " + balls[1] +"ball3: " + balls[2] +"\n";
   // output.innerHTML += "ball1wid: " + balls[0].clientWidth + "ball2wid: " + balls[1].clientWidth+"ball3wid: " + balls[2].clientWidth +"\n";
   // output.innerHTML += "ball1wid: " + balls[0].clientHeight + "ball2wid: " + balls[1].clientHeight+"ball3wid: " + balls[2].clientHeight +"\n";
   // output.innerHTML += "gardW: " + garden.clientWidth + "gardH: " + garden.clientHeight +"\n";
-  output.innerHTML  += "12: "+"\n";
+  output.innerHTML  += "13: "+"\n";
 
   // Because we don't want to have the device upside down
   // We constrain the x value to the range [-90,90]
@@ -51,7 +51,9 @@ function handleOrientation(event) {
   if ((speed + x_px < 180) && (speed + x_px > 0)){
     balls[i].style.left  = x_px + x*speed + "px";  //200 -100 -5
   }
-    }
+  setTimeout(function(){}, 1000);
+
+}
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
