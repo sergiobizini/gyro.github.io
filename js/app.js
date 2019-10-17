72152aac7eecbeca6658048c7b86a584ec9bf172
sergiobizini <sergiobizini@gmail.com>
2019-10-17 19:18:12 +0300

    d

--- js/app.js
+++ js/app.js
@@ -18,10 +18,10 @@ function handleOrientation(event) {
   output.innerHTML = new Date().getTime() + "\n";
 
   for (i = 0; i < balls.length; i++) {
-    y_px = (balls[i].style.top.match(/\d+/));
-    x_px = (balls[i].style.left.match(/\d+/));
+    y_px = balls[i].style.top.match(/\d+/);
+    x_px = balls[i].style.left.match(/\d+/);
 
-    output.innerHTML += "x: "+x_px+"\n";
+    output.innerHTML += "x: " + x_px + "i: " + i "\n";
     output.innerHTML += "y: "+y_px+"\n";
   }
   sleep(1000);
